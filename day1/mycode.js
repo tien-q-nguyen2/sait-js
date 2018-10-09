7910b1e3ab9e247be4cfae90f45c8757e4d579e9
Tien Quang Nguyen <tien-q-nguyen2@gmail.com>
2018-10-09 08:52:45 -0600

    create external mycode.js and link index.html to it

---
+++ day1/mycode.js
@@ -0,0 +1,8 @@
+alert('Hello World!');
+console.log('Secret message');
+// This is a comment
+/* This is also a comment but it takes
+up 2 lines */
+alert('This is an alert');
+console.log('This is a message to the console');
+document.write("This document belongs to Tien.");
